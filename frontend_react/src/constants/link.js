128a590f1721603805999e2644cd6a04909b6607
Yemelyanov <nikolaef.post@gmail.com>
2023-04-02 14:53:52 +0300

    edit edit resume file 2.04.23 LINKEDIN link

--- frontend_react/src/constants/link.js
+++ frontend_react/src/constants/link.js
@@ -1,3 +1,3 @@
 export const FACEBOOK = 'https://www.facebook.com/nikolaeff.post/'
 export const INSTA = 'https://www.instagram.com/nikolaeff.post/'
-export const LINKEDIN =  'https://www.linkedin.com/in/%D1%8D%D0%B4%D1%83%D0%B0%D1%80%D0%B4-%D0%B5%D0%BC%D0%B5%D0%BB%D1%8C%D1%8F%D0%BD%D0%BE%D0%B2-586a96204/'
+export const LINKEDIN =  'https://www.linkedin.com/in/eduard-yemelyanov-586a96204/'

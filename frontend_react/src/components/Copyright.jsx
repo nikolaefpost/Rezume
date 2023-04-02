c4289013924fcb790fe620f331fa3df52b74fd5c
Yemelyanov <nikolaef.post@gmail.com>
2023-04-02 15:31:52 +0300

    fix: edit Copyright

--- frontend_react/src/components/Copyright.jsx
+++ frontend_react/src/components/Copyright.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Copyright = () => {
+    const date = new Date();
+
     return (
         <div className='copyright'>
-            <p className='p-text'>© 2022 Pilot</p>
+            <p className='p-text'>© {date.getFullYear()} Pilot</p>
             <p className='p-text'>All right reserved</p>
         </div>
     );
